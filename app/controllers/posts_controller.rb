ec8ba9111874ecdc4b6bac49c61733e1ead9adc7
Zecharias Mengis <zmengis@gmail.com>
2021-04-16 09:37:54 -0500

    commit tree e8d16907edbebccc267e8b1d01342fcaef5f48ac

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,16 +12,16 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
+		@post = Post.new(params.require(:post).permit(:title, :description))
+		@post.save
+		redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
-	end
+		@post = Post.find(params[:id])
+		@post.update(params.require(:post).permit(:title))
+		redirect_to post_path(@post)
+	end 
 
 	def edit
 	  @post = Post.find(params[:id])
